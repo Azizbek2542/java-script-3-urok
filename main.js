4fb29444a79a7a772bbc369e411c8f84a626ea67
Azizbek2542 <tuhtasinova.x@gmail.com>
2025-07-09 22:51:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+alert("Sizda hozir random sonlar chiqishni boshlaydi 😉")
+let random = Math.round(Math.random() * 70 );
+let result = random * 2;
+let secondResult = random / 2;
+let thirdResult = random /3;
+alert(`Random orqali chiqqan son: ${random}`);
+confirm("Biz random sonni 2 ga ko'paytirmoqchimiz va bo'lmoqchimiz !");
+alert(`Random orqali chiqqan son: ${random} edi
+biz uni 2 ga ko'paytirib: ${result} sonini chiqardik,
+va shu sonni 2 ga bo'lib: ${secondResult} sonini chiqardik,
+va shu sonni 3 ga bo'lgandagi qoldik: ${thirdResult} sonini chiqardik.
+`)
+
+console.log(`Random orqali chiqqan son: ${random} Random orqali chiqqan son: ${random} edi,
+biz uni 2 ga ko'paytirib ${result} sonini chiqardik, 
+va shu sonni 2 ga bo'lib: ${secondResult} sonini chiqardik, 
+va shu sonni 3 ga bo'lgandagi qoldik: ${thirdResult} sonini chiqardik.
+`)
+
